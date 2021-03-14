5b5c05cc0e70234dc19b98c4b241d04fb4a7bbcd
Felipe Alves <felipealves0891@gmail.com>
2021-03-14 19:24:56 -0300

    first commit

---
+++ ChatHubV2/Hubs/Chat.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatHubV2.Hubs
+{
+    [Authorize]
+    public class Chat : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("newMessage", Context.User.Identity.Name, message);
+        }
+    }
+}
---
+++ ChatHubV2/wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const messageForm = document.getElementById('message-form');
+const messageBox = document.getElementById('message-box');
+const messages = document.getElementById('messages');
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chat")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+connection.on('newMessage', (sender, messageText) => {
+    console.log(`${sender}:${messageText}`);
+
+    const newMessage = document.createElement('li');
+    newMessage.appendChild(document.createTextNode(`${sender}:${messageText}`));
+    messages.appendChild(newMessage);
+});
+
+connection.start()
+    .then(() => console.log('connected!'))
+    .catch(console.error);
+
+messageForm.addEventListener('submit', ev => {
+    ev.preventDefault();
+    const message = messageBox.value;
+    connection.invoke('SendMessage', message);
+    messageBox.value = '';
+});
